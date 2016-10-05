31a3f9ec5788a529b7f4484059085b5c8f71026e
James Freeman <freeman.james.h@gmail.com>
2016-10-05 08:10:30 -0600

    set up database

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../db/knex');
 
-
-let userlist = [
-  {
-    username: 'SugarRay113',
-    email:'sugarray113@aol.com',
-    firstname: 'James',
-    lastname: 'Freeman',
-    password: 'markmcgrath4eva'
-
-  },
-  {
-    username: 'CarolinaCoderD00D',
-    email: 'mattyb@galvanize.com',
-    firstname: 'Matthew',
-    lastname: 'Boo',
-    password: 'banjos4lifeDJ3d09'
-  },
-  {
-    username: 'HerokuHuntress2k',
-    email: 'ekope@galvanize.com',
-    firstname: 'Elana',
-    lastname: 'Kopelevich',
-    password: '#staplebatteryhorseNINTENDO'
-  },
-  {
-    username: 'CraiggoMYEggo',
-    email: 'craig.quincy@galvanize.com',
-    firstname: 'Craig',
-    lastname: 'Quincy',
-    password: 'password123'
-  }
-]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('home', ({userlist: userlist}));
+  knex('users').then(function(results){
+    res.render('home', ({userlist: results}));
+  })
 });
 
 router.post('/signup', function (req, res, next) {
   let userObj = req.body;
-  userlist.push(userObj)
-  console.log(`Here's the req.body:`, req.body);
-  res.redirect('/')
+  knex('users').insert(userObj).then(function(){
+    res.redirect('/')
+  })
 })
 
 module.exports = router;
